76f8c071d233ba93561ce57b43f7cfaa4ba147ec
motazabdou <79525259+motazabdou@users.noreply.github.com>
2021-07-04 10:28:10 +0100

    Update meakly.js

--- assets/scripts/meakly.js
+++ assets/scripts/meakly.js
@@ -26,7 +26,7 @@ function getSurahsMeakly(){
 
         allSurahs.forEach((surah,index) => {
             surah.addEventListener("click", function(){
-                fetch(`http://api.alquran.cloud/v1/surah/${index + 1}/ar.mahermuaiqly`)
+                fetch(`https://api.alquran.cloud/v1/surah/${index + 1}/ar.mahermuaiqly`)
                 .then(response => response.json())
                 .then(data => {
                     let verses = data.data.ayahs;
